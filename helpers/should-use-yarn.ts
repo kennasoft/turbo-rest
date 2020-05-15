7c491f5b09727c8d43e76bb006210bb33efc9e96
Ikenna Okonkwo <kennasoft@Ikennas-MacBook-Pro.local>
2020-05-15 17:51:17 -0400

    add utilities from create-next-app

---
+++ helpers/should-use-yarn.ts
@@ -0,0 +1,18 @@
+/*
+  Thanks to next.js team for this utility
+  https://github.com/zeit/next.js/blob/master/packages/create-next-app/helpers/should-use-yarn.ts
+*/
+import { execSync } from "child_process";
+
+export function shouldUseYarn(): boolean {
+  try {
+    const userAgent = process.env.npm_config_user_agent;
+    if (userAgent) {
+      return Boolean(userAgent && userAgent.startsWith("yarn"));
+    }
+    execSync("yarnpkg --version", { stdio: "ignore" });
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
