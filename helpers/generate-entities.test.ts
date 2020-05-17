327b47edffdfcbd4d35c7582ef60dc664dc1d918
Ikenna Okonkwo <kennasoft@Ikennas-MacBook-Pro.local>
2020-05-17 18:45:35 -0400

    add unit tests

---
+++ helpers/generate-entities.test.ts
@@ -0,0 +1,54 @@
+import { TmgConfig, generateEntities } from "./generate-entities";
+import * as spawn from "cross-spawn";
+
+const crossSpawnMock = (
+  command: string,
+  args?: string[],
+  options?: Record<string, any>
+) => ({
+  command,
+  args,
+  on: (event: string, callback: Function) => undefined,
+});
+jest.mock("cross-spawn", () => ({ default: crossSpawnMock }));
+jest.mock("make-dir");
+jest.mock("fs");
+
+describe("generateEntities()", () => {
+  const mockConfig: TmgConfig = {
+    host: "localhost",
+    port: "3306",
+    engine: "mysql",
+    database: "",
+    user: "",
+    pass: "",
+    skipTables: "",
+  };
+
+  let mockExit: jest.SpyInstance;
+  let mockConsoleLog: jest.SpyInstance;
+  beforeEach(() => {
+    mockExit = jest
+      .spyOn(process, "exit")
+      // @ts-ignore
+      .mockImplementation((code?: number) => code);
+    mockConsoleLog = jest
+      .spyOn(console, "log")
+      // @ts-ignore
+      .mockImplementation((text: string) => text);
+  });
+
+  afterEach(() => {
+    mockExit.mockRestore();
+    mockConsoleLog.mockRestore();
+  });
+
+  it("should invoke typeorm-model-generator with no args if only default args present", async () => {
+    try {
+      await generateEntities("", mockConfig);
+    } catch {}
+    expect(mockConsoleLog).toHaveBeenCalledWith(
+      "spawning process npx with args [typeorm-model-generator]"
+    );
+  });
+});
