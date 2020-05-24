f96742b2cdd09b15ce1840cdaf8400df36ae0518
Ikenna Okonkwo <kennasoft@Ikennas-MacBook-Pro.local>
2020-05-24 18:56:54 -0400

    add lerna and yarn workspaces support

---
+++ packages/restalize/helpers/link-template-files.ts
@@ -0,0 +1,28 @@
+import fs from "fs";
+import path from "path";
+
+// const handleError = (err: NodeJS.ErrnoException | null) =>
+//   err && console.error(err);
+const pathToAutoRest = require.resolve("@restalize/auto-rest");
+const typeormTemplateRoot = path.join(__dirname, "..", "templates", "typeorm");
+const autoRestRoot = pathToAutoRest.replace(/\/dist\/server.js$/, "");
+
+export default async function linkTemplateFiles(
+  serverType: string
+): Promise<string | void> {
+  return new Promise((resolve, reject) => {
+    if (fs.existsSync(typeormTemplateRoot)) {
+      return resolve();
+    }
+    fs.symlink(autoRestRoot, typeormTemplateRoot, (err) => {
+      if (err) {
+        reject(err.message);
+        return err.message;
+      }
+      console.log(`template files symlinked at "${typeormTemplateRoot}"`);
+      return resolve();
+    });
+  });
+}
+
+linkTemplateFiles("").then((err) => console.error(err));
