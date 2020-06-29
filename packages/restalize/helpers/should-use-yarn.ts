0a377df2e8d542722bea929cc96195a8a7bbc7f7
Ikenna Okonkwo <kennasoft@Ikennas-MacBook-Pro.local>
2020-06-29 18:55:15 -0400

    add istanbul ignore to @zeit utilities

--- packages/restalize/helpers/should-use-yarn.ts
+++ packages/restalize/helpers/should-use-yarn.ts
@@ -2,6 +2,7 @@
   Thanks to next.js team for this utility
   https://github.com/zeit/next.js/blob/master/packages/create-next-app/helpers/should-use-yarn.ts
 */
+/* istanbul ignore file */
 import { execSync } from "child_process";
 
 export function shouldUseYarn(): boolean {
